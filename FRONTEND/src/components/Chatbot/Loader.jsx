2b387a15bc7ec3d8cd8bfb4c62d109217bf78554
vinay02022 <vinay.work25@gmail.com>
2026-01-16 21:36:22 +0530

    Add loading indicator

---
+++ FRONTEND/src/components/Chatbot/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="message-bubble bot">
+      <div className="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
